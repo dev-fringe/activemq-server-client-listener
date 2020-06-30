9a4935f6bd58256735e0f49fd1e26039aa9ce8e1
Lee Hak Do <dev.fringe@gmail.com>
2020-06-30 18:09:53 +0900

    init

--- src/main/java/dev/fringe/activemq/Client.java
+++ src/main/java/dev/fringe/activemq/Client.java
@@ -12,26 +12,25 @@ import dev.fringe.activemq.config.ActiveMqCommonConfig;
 import dev.fringe.activemq.model.Email;
 
 @Configuration
-@Import(value = {ActiveMqCommonConfig.class, ActiveMqClientConfig.class})
-public class Client implements InitializingBean{
+@Import(value = { ActiveMqCommonConfig.class, ActiveMqClientConfig.class })
+public class Client implements InitializingBean {
 
-    @Autowired JmsTemplate jmsTemplate;
-    @Autowired JmsTemplate jmsTemplate2;
-    
-    
-    public static void main(String[] args) {
-    	new AnnotationConfigApplicationContext(Client.class);   
-    }
+	@Autowired JmsTemplate jmsTemplate;
+	@Autowired JmsTemplate jmsTemplate2;
+
+	public static void main(String[] args) {
+		new AnnotationConfigApplicationContext(Client.class);
+	}
 
 	public void afterPropertiesSet() throws Exception {
-        Email email = new Email();
-        email.setToRecipient("info@email.com");
-        email.setSubject("Subject");
-        email.setEmailBody("Hello");
-        try {
-        	jmsTemplate.convertAndSend("mailbox", email);
+		Email email = new Email();
+		email.setToRecipient("info@email.com");
+		email.setSubject("Subject");
+		email.setEmailBody("Hello");
+		try {
+			jmsTemplate.convertAndSend("mailbox", email);
 		} catch (Exception e) {
-        	jmsTemplate2.convertAndSend("mailbox", email);
+			jmsTemplate2.convertAndSend("mailbox", email);
 		}
 	}
 }
